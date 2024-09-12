8bc9af79652aaa9315b949fb4eadfc293be82810
yongho <yongho612@gmail.com>
2024-09-12 23:10:12 +0900

    initial

---
+++ src/main/java/com/flutter/DataPreprocessingService/DataPreprocessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flutter.DataPreprocessingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPreprocessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPreprocessingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flutter/DataPreprocessingService/DataPreprocessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flutter.DataPreprocessingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPreprocessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
